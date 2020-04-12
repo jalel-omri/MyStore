fa61c4f14da920af34f5fdf2b4e2b084b0c45cdf
jalel-omri <jalel.omri@ensi-uma.tn>
2020-04-12 23:32:04 +0100

    new commit

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/MyStore'));
 // default Heroku PORT
 app.listen(process.env.PORT || 8080);
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/MyStore/index.html'));
 });
 
